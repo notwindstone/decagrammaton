700e163e0871bd656455470612885d2c4dec3c90
notwindstone <notwindstone@gmail.com>
2026-07-07 01:45:30 +0300

    feat: use 'ark-of-atrahasis' package for the safe DOM wrapper

--- examples/src/main.ts
+++ examples/src/main.ts
@@ -4,9 +4,9 @@ import "virtual:uno.css";
 // Resets all styles in a Tailwind-like way
 import "@unocss/reset/tailwind.css";
 
-import { createSafeDocument } from "../../src/__temporary/gui";
 import { createApp } from "../../src";
 import App from "./app.deca";
+import { createSafeDocument } from "ark-of-atrahasis";
 
 const gui = createSafeDocument(
   document.getElementById("app")!
